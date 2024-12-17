9221e22b80dad3e07ae8fb03eca512eac63eb6ca
HenryToti <henryokoth3@gmail.com>
2024-12-17 15:20:00 +0300

    Initial Commit

---
+++ ItunesSearch/ItunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesSearchApp.swift
+//  ItunesSearch
+//
+//  Created by Billy Okoth on 17/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ItunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
